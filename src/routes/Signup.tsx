3d4520131cad09bfcc0a8e395a40a4e8eae566b5
benkim077 <benkim0077@gmail.com>
2023-06-11 17:26:53 +0900

    Feat: 회원가입 페이지 마크업

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -2,5 +2,16 @@ import React from 'react';
 import style from './Signup.module.css';
 
 export default function Signup() {
-  return <h1>Signup</h1>;
+  return (
+    <>
+      <h1>회원가입</h1>
+      <form>
+        <input type='text' placeholder='Email' data-testid='email-input' />
+        <input type='password' placeholder='Password' data-testid='password-input' />
+        <button type='submit' data-testid='signup-button'>
+          회원가입
+        </button>
+      </form>
+    </>
+  );
 }
