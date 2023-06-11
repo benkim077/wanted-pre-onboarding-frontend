45d54a794cb93610af72b178b233b65c35b0c953
benkim077 <benkim0077@gmail.com>
2023-06-11 17:35:40 +0900

    Feat: 회원가입 유효성 검사 구현

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -14,13 +14,25 @@ export default function Signup() {
     setPassword(value);
   };
 
+  const validateSignUpForm = (email: string, password: string) => {
+    return validateEmail(email) && validatePassword(password);
+
+    function validateEmail(email: string) {
+      return email.includes('@');
+    }
+
+    function validatePassword(password: string) {
+      return password.length >= 8;
+    }
+  };
+
   return (
     <>
       <h1>회원가입</h1>
       <form>
-        <input type='text' placeholder='Email' data-testid='email-input' onChange={handleChangeEmail} />
-        <input type='password' placeholder='Password' data-testid='password-input' onChange={handleChangePassword} />
-        <button type='submit' data-testid='signup-button'>
+        <input type='text' placeholder='Email' data-testid='email-input' onChange={handleChangeEmail} value={email} />
+        <input type='password' placeholder='Password' data-testid='password-input' onChange={handleChangePassword} value={password} />
+        <button type='submit' data-testid='signup-button' disabled={!validateSignUpForm(email, password)}>
           회원가입
         </button>
       </form>
