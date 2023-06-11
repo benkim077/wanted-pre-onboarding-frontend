0cd45b8bd138413a4ae19d4671d055d13f6d93aa
benkim077 <benkim0077@gmail.com>
2023-06-11 17:33:09 +0900

    Feat: 회원가입에 사용할 상태 관리

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import style from './Signup.module.css';
+import React, { ChangeEvent, useState } from 'react';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setEmail(value);
+  };
+
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setPassword(value);
+  };
+
   return (
     <>
       <h1>회원가입</h1>
       <form>
-        <input type='text' placeholder='Email' data-testid='email-input' />
-        <input type='password' placeholder='Password' data-testid='password-input' />
+        <input type='text' placeholder='Email' data-testid='email-input' onChange={handleChangeEmail} />
+        <input type='password' placeholder='Password' data-testid='password-input' onChange={handleChangePassword} />
         <button type='submit' data-testid='signup-button'>
           회원가입
         </button>
