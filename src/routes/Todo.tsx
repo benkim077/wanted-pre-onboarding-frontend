3ba2715c33e7e7dd490ff2ce8abfb081e35cd3e7
benkim077 <benkim0077@gmail.com>
2023-06-12 00:59:10 +0900

    Feat: 할 일 추가 기능 구현

--- src/routes/Todo.tsx
+++ src/routes/Todo.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TodoItem } from '@Types/todo';
 import List from '@Components/List';
 
 export default function Todo() {
-  const todos: TodoItem[] = [
+  const [todos, setTodos] = useState<TodoItem[]>([
     { id: 1, todo: 'Buy milk', isCompleted: false, userId: 1 },
     { id: 2, todo: 'Buy eggs', isCompleted: false, userId: 1 },
     { id: 3, todo: 'Buy bread', isCompleted: false, userId: 1 },
-  ];
+  ]);
+
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleNewTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(e.target.value);
+  };
 
   const navigate = useNavigate();
 
@@ -23,16 +29,18 @@ export default function Todo() {
   return (
     <>
       <h1>Todo</h1>
+      <input type='text' placeholder='할 일 입력' data-testid='new-todo-input' value={newTodo} onChange={handleNewTodoChange} />
+      <button type='button' data-testid='new-todo-add-button'>
+        추가
+      </button>
       <List
         list={todos}
         renderItem={(item) => {
           return (
-            <li>
-              <label>
-                <input type='checkbox' value={String(item.isCompleted)} />
-                <span>{item.todo}</span>
-              </label>
-            </li>
+            <label>
+              <input type='checkbox' value={String(item.isCompleted)} />
+              <span>{item.todo}</span>
+            </label>
           );
         }}
       />
