27b742e1f80e032b31a3a661b6d4a4957980eb2d
benkim077 <benkim0077@gmail.com>
2023-06-12 11:10:16 +0900

    Feat: Todo checkbox 클릭 시 콜백함수 구현
    - updateTodo API 요청
    - 요청 response를 받아서 todos를 업데이트

--- src/routes/Todo.tsx
+++ src/routes/Todo.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useLoaderData } from 'react-router-dom';
 import { TodoItem } from '@Types/todo';
 import List from '@Components/List';
-import { createTodo, getTodos } from '@Apis/todo';
+import { createTodo, getTodos, updateTodo } from '@Apis/todo';
 
 export default function Todo() {
   const initial_todos = useLoaderData() as TodoItem[];
@@ -20,6 +20,27 @@ export default function Todo() {
     const { data } = await createTodo(newTodo);
 
     setTodos([...todos, data]);
+  };
+
+  const handleTodoChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const id = Number(event.target.dataset.id);
+
+    const updatedTodo = todos.find((todo) => todo.id === id);
+
+    if (!updatedTodo) {
+      return;
+    }
+
+    const { data } = await updateTodo(id, updatedTodo.todo, !updatedTodo.isCompleted);
+
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return data;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
   };
 
   const navigate = useNavigate();
@@ -47,7 +68,7 @@ export default function Todo() {
           return (
             <li>
               <label>
-                <input type='checkbox' value={String(item.isCompleted)} />
+                <input type='checkbox' data-id={item.id} value={String(item.isCompleted)} onChange={handleTodoChange} />
                 <span>{item.todo}</span>
               </label>
             </li>
