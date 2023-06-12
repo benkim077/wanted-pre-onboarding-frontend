1c7ca0de14f03effd40d8607b970ed380f2f6c75
benkim077 <benkim0077@gmail.com>
2023-06-12 19:19:18 +0900

    Feat: Item 컴포넌트 조건부 렌더링, 핸들러 함수 구현
    - isModificationMode 상태값을 바탕으로, 조건부 렌더링
    - 수정, 삭제, 제출, 취소 버튼에 대한 핸들러 함수 구현

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoItem } from '@Types/todo';
 
 interface ItemProps {
@@ -10,18 +10,61 @@ interface ItemProps {
 export default function Item({ item, handleUpdateTodo, handleDeleteTodo }: ItemProps) {
   const { id, todo, isCompleted } = item;
 
+  const [inputTodo, setInputTodo] = useState(todo);
+
+  const handleNewTodoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputTodo(event.target.value);
+  };
+
+  const [isModificationMode, setIsModificationMode] = useState(false);
+
+  const handleUpdateButtonClick = () => {
+    setIsModificationMode(!isModificationMode);
+  };
+
+  const handleSubmitButtonClick = () => {
+    handleUpdateTodo(id, inputTodo, isCompleted)();
+
+    setIsModificationMode(!isModificationMode);
+  };
+
+  const handleCancelButtonClick = () => {
+    setInputTodo(todo);
+
+    setIsModificationMode(!isModificationMode);
+  };
+
   return (
     <li>
-      <label>
-        <input type='checkbox' checked={isCompleted} onClick={handleUpdateTodo(id, todo, !isCompleted)} />
-        <span>{item.todo}</span>
-      </label>
-      <button type='button' data-testid='modify-button'>
-        수정
-      </button>
-      <button type='button' data-testid='delete-button' onClick={handleDeleteTodo(item.id)}>
-        삭제
-      </button>
+      {isModificationMode && (
+        <>
+          <label>
+            <input type='checkbox' defaultChecked={isCompleted} onClick={handleUpdateTodo(id, todo, !isCompleted)} />
+            <input type='text' value={inputTodo} onChange={handleNewTodoChange} data-testid='modify-input' />
+          </label>
+          <button type='button' data-testid='submit-button' onClick={handleSubmitButtonClick}>
+            제출
+          </button>
+          <button type='button' data-testid='cancel-button' onClick={handleCancelButtonClick}>
+            취소
+          </button>
+        </>
+      )}
+
+      {!isModificationMode && (
+        <>
+          <label>
+            <input type='checkbox' defaultChecked={isCompleted} onClick={handleUpdateTodo(id, todo, !isCompleted)} />
+            {!isModificationMode && <span>{item.todo}</span>}
+          </label>
+          <button type='button' data-testid='modify-button' onClick={handleUpdateButtonClick}>
+            수정
+          </button>
+          <button type='button' data-testid='delete-button' onClick={handleDeleteTodo(item.id)}>
+            삭제
+          </button>
+        </>
+      )}
     </li>
   );
 }
